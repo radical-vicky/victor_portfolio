2cd8abffe4583d1216de98bc8d03a079e9bb2b30
Victor Odhiambo <146205165+radical-vicky@users.noreply.github.com>
2026-03-26 23:25:52 +0300

    Update context_processors.py

--- portfolio/context_processors.py
+++ portfolio/context_processors.py
@@ -1,7 +1,11 @@
-from .models import About
+from .models import About, Project, Skill, ContactMessage
 
 def about_context(request):
-    """Add about object to all templates automatically"""
+    """Add about object and other global data to all templates automatically"""
     return {
-        'about': About.objects.first()
+        'about': About.objects.first(),
+        'total_projects': Project.objects.count(),
+        'total_skills': Skill.objects.count(),
+        'total_messages': ContactMessage.objects.count(),
+        'unread_messages': ContactMessage.objects.filter(is_read=False).count(),
     }
