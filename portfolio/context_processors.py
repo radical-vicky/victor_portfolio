1a6beda158f0d541efc414a8a535344081453fab
radical-vicky <entreprenuerridicular@gmail.com>
2026-03-27 16:30:09 +0300

    first commit

--- portfolio/context_processors.py
+++ portfolio/context_processors.py
@@ -1,11 +1,12 @@
-from .models import About, Project, Skill, ContactMessage
+from .models import About, Project, Skill, ContactMessage, Testimonial
 
 def about_context(request):
-    """Add about object and other global data to all templates automatically"""
+    """Add about object and stats to all templates"""
     return {
         'about': About.objects.first(),
         'total_projects': Project.objects.count(),
         'total_skills': Skill.objects.count(),
         'total_messages': ContactMessage.objects.count(),
+        'total_testimonials': Testimonial.objects.filter(is_approved=True).count(),
         'unread_messages': ContactMessage.objects.filter(is_read=False).count(),
     }
